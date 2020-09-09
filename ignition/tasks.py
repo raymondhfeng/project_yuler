4b336532ba6cb246cde4df2e76e7e1ebf58a7432
Raymond Feng <raymondhfeng@berkeley.edu>
2020-09-08 23:10:19 -0700

    added some clunky post processing for when there are ocr errors

--- ignition/tasks.py
+++ ignition/tasks.py
@@ -21,43 +21,44 @@ from datetime import datetime
 @task()
 def task_number_one():
 
-	num_ppl, avg_pot, plrs_flop = get_stats()
-	for i in range(len(num_ppl)):
-		num_ppl[i] = num_ppl[i].replace('+','')
-		try:
-			num_ppl[i] = int(num_ppl[i])
-		except ValueError:
-			num_ppl[i] = -1
-	for i in range(len(avg_pot)):
-		avg_pot[i] = avg_pot[i].replace('$','').strip('.')
-		try:
-			avg_pot[i] = float(avg_pot[i])
-		except ValueError:
-			avg_pot[i] = -1
-	for i in range(len(plrs_flop)):
-		plrs_flop[i] = plrs_flop[i].replace('%','')
-		try:
-			plrs_flop[i] = int(plrs_flop[i])
-		except ValueError:
-			plrs_flop[i] = -1
-	pct_flop = plrs_flop	
-	d = IgnitionRow(num_players_5=num_ppl[0],
-					num_players_25=num_ppl[1],
-					num_players_50=num_ppl[2],
-					num_players_200=num_ppl[3],
-					num_players_500=num_ppl[4],
-					avg_pot_5=avg_pot[0],
-					avg_pot_25=avg_pot[1],
-					avg_pot_50=avg_pot[2],
-					avg_pot_200=avg_pot[3],
-					avg_pot_500=avg_pot[4],
-					pct_flop_5=pct_flop[0],
-					pct_flop_25=pct_flop[1],
-					pct_flop_50=pct_flop[2],
-					pct_flop_200=pct_flop[3],
-					pct_flop_500=pct_flop[4],
-					pub_date=timezone.now())
-	d.save()
+        num_ppl, avg_pot, plrs_flop = get_stats()
+        for i in range(len(num_ppl)):
+                num_ppl[i] = num_ppl[i].replace('+','')
+                try:
+                        num_ppl[i] = int(num_ppl[i])
+                except ValueError:
+                        num_ppl[i] = -1
+        for i in range(len(avg_pot)):
+                avg_pot[i] = avg_pot[i].replace('$','').strip('.')
+                try:
+                        avg_pot[i] = float(avg_pot[i])
+                except ValueError:
+                        avg_pot[i] = -1
+        for i in range(len(plrs_flop)):
+                plrs_flop[i] = plrs_flop[i].replace('%','')
+                plrs_flop[i] = plrs_flop[i][:2] # TODO: This is clunky
+                try:
+                        plrs_flop[i] = int(plrs_flop[i])
+                except ValueError:
+                        plrs_flop[i] = -1
+        pct_flop = plrs_flop    
+        d = IgnitionRow(num_players_5=num_ppl[0],
+                                        num_players_25=num_ppl[1],
+                                        num_players_50=num_ppl[2],
+                                        num_players_200=num_ppl[3],
+                                        num_players_500=num_ppl[4],
+                                        avg_pot_5=avg_pot[0],
+                                        avg_pot_25=avg_pot[1],
+                                        avg_pot_50=avg_pot[2],
+                                        avg_pot_200=avg_pot[3],
+                                        avg_pot_500=avg_pot[4],
+                                        pct_flop_5=pct_flop[0],
+                                        pct_flop_25=pct_flop[1],
+                                        pct_flop_50=pct_flop[2],
+                                        pct_flop_200=pct_flop[3],
+                                        pct_flop_500=pct_flop[4],
+                                        pub_date=timezone.now())
+        d.save()
 
 @task()
 def task_number_two():
